b0fd93a4c0d1b8dcfeb5e4b72b9377a4dc78aedf
zhangqi6627 <356480127@qq.com>
2016-08-16 23:35:15 +0800

    更新LineGridView

--- CarCare/src/com/qust/zq/carcare/view/LineGridView.java
+++ CarCare/src/com/qust/zq/carcare/view/LineGridView.java
@@ -11,7 +11,6 @@ import com.qust.zq.carcare.R;
 public class LineGridView extends GridView {
 	public LineGridView(Context context) {
 		super(context);
-		// TODO Auto-generated constructor stub
 	}
 	public LineGridView(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -24,11 +23,11 @@ public class LineGridView extends GridView {
 		super.dispatchDraw(canvas);
 		View localView1 = getChildAt(0);
 		int column = 0;
-		if(localView1 != null){
+		if (localView1 != null) {
 			column = getWidth() / localView1.getWidth();// 计算出一共有多少列，假设有3列
 		}
 		int childCount = getChildCount();// 子view的总数
-		System.out.println("子view的总数childCount==" + childCount+" "+column);
+		System.out.println("子view的总数childCount==" + childCount + " " + column);
 		Paint localPaint;// 画笔
 		localPaint = new Paint();
 		localPaint.setStyle(Paint.Style.STROKE);
@@ -36,17 +35,22 @@ public class LineGridView extends GridView {
 		for (int i = 0; i < childCount; i++) {// 遍历子view
 			View cellView = getChildAt(i);// 获取子view
 			if (i < column) {// 第一行
-				//top line
+				// top line
 				canvas.drawLine(cellView.getLeft(), cellView.getTop(), cellView.getRight(), cellView.getTop(), localPaint);
 			}
 			if (i % column == 0) {// 第一列
-				//left line
+				// left line
 				canvas.drawLine(cellView.getLeft(), cellView.getTop(), cellView.getLeft(), cellView.getBottom(), localPaint);
 			}
 			// bottom line
-			canvas.drawLine(cellView.getLeft(), cellView.getBottom() -1, cellView.getRight(), cellView.getBottom()-1, localPaint);
+			canvas.drawLine(cellView.getLeft(), cellView.getBottom() - 1, cellView.getRight(), cellView.getBottom() - 1, localPaint);
 			// right line
-			canvas.drawLine(cellView.getRight()-1, cellView.getTop(), cellView.getRight()-1, cellView.getBottom(), localPaint);
+			canvas.drawLine(cellView.getRight() - 1, cellView.getTop(), cellView.getRight() - 1, cellView.getBottom(), localPaint);
 		}
+	}
+	@Override
+	public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int expandSpec = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+		super.onMeasure(widthMeasureSpec, expandSpec);
 	}
 }
